6acda1f0203ece0ded269adba657fc9c2fda3325
Anton Sushchenko <vailant0694@gmail.com>
2022-02-08 22:09:08 +0200

    main

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector('body');
+const buttonRef = document.querySelector('button');
+const spanRef = document.querySelector('span');
+
+buttonRef.addEventListener('click', onBodyColorChange);
+
+
+function onBodyColorChange() {
+  const newColor = getRandomHexColor();
+  document.body.setAttribute('style', `background-color: ${newColor}`);
+  spanRef.textContent = newColor;
+};
