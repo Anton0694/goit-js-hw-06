d007439698399ec1d3cca552f831b5dcebf1d955
Anton0694 <91780319+Anton0694@users.noreply.github.com>
2022-11-20 21:38:36 +0200

    4taska

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -2,15 +2,16 @@ const counterEl = document.querySelector('#counter')
 const counterValue = document.querySelector('#value')
 let count = 0;
 function btnDecr(event) {
-    event.currentTarget.value -= 1;
+    event.currentTarget.value = count;
+    count -= 1; 
     counterValue.textContent = event.currentTarget.value;
+    /* event.currentTarget.value -= 1;
+    counterValue.textContent = event.currentTarget.value; */
 }
 
 function btnIncr(event) {
     event.currentTarget.value = count;
     count += 1; 
-    
-    /* console.log(counterValue.textContent); */
     counterValue.textContent = event.currentTarget.value;
 }
 
