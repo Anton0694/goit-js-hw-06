2bf85d4010c4fb4b17bf78d8ae1a0c203f88cab6
Anton0694 <91780319+Anton0694@users.noreply.github.com>
2022-11-25 14:42:37 +0200

    9th

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,7 +1,7 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const colorRandomizer = getRandomHexColor();
+
 const refs = {
   widgetEl: document.querySelector(".widget"),
   span: document.querySelector(".color"),
@@ -10,6 +10,7 @@ const refs = {
 };
 refs.button.addEventListener("click", onButtonName);
 function onButtonName(event) {
+  let colorRandomizer = getRandomHexColor();
   refs.span.textContent = colorRandomizer;
   document.body.style.backgroundColor = colorRandomizer;
 }
